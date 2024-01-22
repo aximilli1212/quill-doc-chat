60b46e8b1f14b45639cc275680bc58f9f1934383
Emmanuel Sackey <aximilli1212@gmail.com>
2024-01-22 06:34:52 +0000

    Add shadcn button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import {ArrowRight} from "lucide-react";
+import {buttonVariants} from "@/components/ui/button";
 
 export default function Home() {
   return (
