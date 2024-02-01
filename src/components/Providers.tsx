8e517a73949c520eeb94e06d286bf496ee7a55af
Afiaansong55 <afiaansong44@gmail.com>
2024-02-01 16:33:18 +0000

    dashboard/upload

---
+++ src/components/Providers.tsx
@@ -0,0 +1,14 @@
+
+"use client"
+
+import { useState } from "react"
+import {QueryClient} from "@tanstack/react-query"
+
+const Providers = () => {
+     const {queryClient} = useState(() => new QueryClient())
+    const {trpcClient} = useState(() => ) 
+
+    console.log(queryClient)
+}
+
+export default Providers
